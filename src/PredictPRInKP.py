29ec8021c6b4f98912ebefaabd11f7b659aa9ad9
Jose Claudio Soares <joseclaudiosoaresneto@gmail.com>
2020-12-21 13:30:33 -0300

    Add confidence interval and feature selection

--- src/PredictPRInKP.py
+++ src/PredictPRInKP.py
@@ -1,21 +1,23 @@
-from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from GwasFeatureExtractor import GwasFeatureExtractor
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.svm import SVC
-from sklearn import metrics
-from sklearn.metrics import plot_roc_curve
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
+from sklearn.feature_selection import RFECV
+from src.GwasFeatureExtractor import GwasFeatureExtractor
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier
+from sklearn.svm import SVC, SVR
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import plot_roc_curve, accuracy_score
 import matplotlib.pyplot as plt
 import numpy as np
+import xgboost as xgb
 import sys
+import random
 import pandas as pd
+import scipy.stats
 
 
 # ################################################## #
 # Author: Jose Claudio Soares                        #
-# Email: joseclaudiosoaresneto@gmail.com              #
+# Email: joseclaudiosoaresneto@gmail.com             #
 # Date(mm/dd/yyyy): 11/03/2020                       #
 # Github: jcsn13                                     #
 # Credits: github.com/crowegian/AMR_ML               #
@@ -27,7 +29,7 @@ class PredictPRinKP:
         self.dict_dataframes = {}
         self.dataset_full = datasets_full
         self.datasets_gwas = datasets_gwas
-        self.random_state = np.random.RandomState(1)
+        self.SEED = np.random.RandomState(42)
 
     def read_datasets(self):
         if self.dataset_full:
@@ -64,30 +66,57 @@ class PredictPRinKP:
     def perform_train_test_split(dataset, metadata):
         if len(dataset) == 1:
             x_train, x_test, y_train, y_test = train_test_split(dataset[0].values, metadata[0].values, test_size=0.25,
-                                                                random_state=42)
+                                                                random_state=42, shuffle=True)
         else:
             gwas_feature_extractor = GwasFeatureExtractor(x_locus_tags=dataset[1]['LOCUS_TAG'], gwas_df=dataset[1])
             gwas_feature_extractor.fit()
             x = gwas_feature_extractor.transform(dataset[0])
-            x = x.drop('isolate', axis=1)
+            # x = x.drop(['isolate'], axis=1)
 
             x_train, x_test, y_train, y_test = train_test_split(x.values, metadata[0].values.ravel(), test_size=0.25,
-                                                                random_state=42)
+                                                                random_state=42, shuffle=True)
 
         return x_train, x_test, y_train, y_test
 
     @staticmethod
-    def print_performance_metrics(grid, name, x_test, y_test, dataset, short_name):
+    def mean_confidence_interval(grid, x_test, y_test, confidence=0.96):
+        score_array = []
+        for i in range(200):
+            x_sample_array = []
+            y_sample_array = []
+
+            range_ = int(len(y_test) * 0.65)
+
+            for j in range(range_):
+                index = np.random.randint(0, len(x_test) - 1)
+                x = x_test[index]
+                y = y_test[index]
+                x_sample_array.append(x)
+                y_sample_array.append(y)
+
+            y_pred = grid.predict(x_sample_array)
+
+            score = accuracy_score(y_pred, y_sample_array)
+            score_array.append(score)
+
+        a = 1.0 * np.array(score_array)
+        n = len(a)
+        m, se = np.mean(a), scipy.stats.sem(a)
+        h = se * scipy.stats.t.ppf((1 + confidence) / 2., n - 1)
+        return m, h
+
+    def print_performance_metrics(self, grid, name, x_test, y_test, dataset, short_name):
         print("\n*********{}*********".format(name))
         print(
             "Performed GridSearchCV 10\nBest CV params {}\nBest CV accuracy {}"
-            "\nTest accuracy of best grid search hypers:"
-            .format(grid.best_params_, grid.best_score_), grid.score(x_test, y_test))
+            "\nTest accuracy(confidence interval 0.95) of best grid search hypers:"
+            .format(grid.best_params_, grid.best_score_), self.mean_confidence_interval(grid, x_test, y_test))
+
+        print(grid.cv_results_)
 
         curve_name = short_name + " " + dataset
-
-        plot_roc_curve(grid, x_test, y_test, name=curve_name)
-        plt.savefig('../plots/'+curve_name)
+        plot_roc_curve(grid.best_estimator_, x_test, y_test, name=curve_name)
+        plt.savefig('../plots/' + curve_name)
         # plt.show()
 
     def perform_train_full(self):
@@ -97,10 +126,13 @@ class PredictPRinKP:
             dataset = [self.dict_dataframes['df_full_' + i]]
             metadata = [self.dict_dataframes['pbr_res_' + i]]
             name = i
+
+            self.perform_feature_selection(dataset, metadata)
+
             self.train_SGDClassifier(dataset, metadata, name)
             self.train_logistic_regression(dataset, metadata, name)
             self.train_SVC(dataset, metadata, name)
-            self.train_random_forest(dataset, metadata, name)
+            # self.train_random_forest(dataset, metadata, name)
             # self.train_GBTC(dataset, metadata, name)
         return self
 
@@ -112,21 +144,37 @@ class PredictPRinKP:
                        self.dict_dataframes['df_gwas_' + i]]
             metadata = [self.dict_dataframes['pbr_res_' + i]]
             name = i
+
+            self.perform_feature_selection(dataset, metadata)
+
             self.train_SGDClassifier(dataset, metadata, name)
             self.train_logistic_regression(dataset, metadata, name)
             self.train_SVC(dataset, metadata, name)
-            self.train_random_forest(dataset, metadata, name)
+            # self.train_random_forest(dataset, metadata, name)
             # self.train_GBTC(dataset, metadata, name)
+        return self
+
+    def perform_feature_selection(self, dataset, metadata):
+        encoder = LabelEncoder()
+        isolate_column = dataset[0].columns[0]
+        dataset[0][isolate_column] = encoder.fit_transform(dataset[0][isolate_column])
+
+        estimator = RandomForestClassifier(random_state=self.SEED)
+        # estimator = SVR(kernel="linear", random_state=self.SEED)
+        selector = RFECV(estimator, step=1, cv=StratifiedKFold(10), scoring='roc_auc')
+        selector = selector.fit(dataset[0], metadata[0].values.ravel())
+
+        selected_features = dataset[0].columns[selector.support_]
+        dataset[0] = dataset[0].filter(selected_features)
         return self
 
     def train_SGDClassifier(self, dataset, metadata, name):
         x_train, x_test, y_train, y_test = self.perform_train_test_split(dataset, metadata)
 
-        parameters = {'alpha': [1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3],
-                      'loss': ['hinge', 'log'], 'penalty': ['l1', 'l2', 'elasticnet'],
-                      'n_jobs': [-1]}
-        model = SGDClassifier(random_state=self.random_state, max_iter=10000)
-        grid = GridSearchCV(model, parameters, cv=10)
+        parameters = {'alpha': [0.001, 0.01, 0.1, 1, 10], 'loss': ['hinge', 'log'],
+                      'penalty': ['l1', 'l2']}
+        model = SGDClassifier(random_state=self.SEED, max_iter=10000)
+        grid = GridSearchCV(model, parameters, cv=10, scoring='accuracy', return_train_score=True)
         grid.fit(x_train, y_train)
 
         model_info = ["Stochastic Gradient Descent Classifier", x_test, y_test, "SGDC"]
@@ -137,10 +185,9 @@ class PredictPRinKP:
     def train_logistic_regression(self, dataset, metadata, name):
         x_train, x_test, y_train, y_test = self.perform_train_test_split(dataset, metadata)
 
-        parameters = {'C': [0.1, 1, 10, 100], 'penalty': ['l2'],
-                      'multi_class': ['auto', 'multinomial']}
-        model = LogisticRegression(random_state=self.random_state, max_iter=10000)
-        grid = GridSearchCV(model, parameters, cv=10)
+        parameters = {'C': [0.001, 0.01, 0.1, 1, 10], 'penalty': ['l2']}
+        model = LogisticRegression(random_state=self.SEED, max_iter=10000)
+        grid = GridSearchCV(model, parameters, cv=10, scoring='accuracy', return_train_score=True)
         grid.fit(x_train, y_train)
 
         model_info = ["Logistic Regression", x_test, y_test, "LR"]
@@ -151,10 +198,10 @@ class PredictPRinKP:
     def train_SVC(self, dataset, metadata, name):
         x_train, x_test, y_train, y_test = self.perform_train_test_split(dataset, metadata)
 
-        parameters = {'C': [0.1, 1, 10, 100], 'gamma': [0.00001, 0.0001, 0.001, 0.01, 0.1],
+        parameters = {'C': [0.1, 1, 10, 100], 'gamma': [0.01, 0.1, 1, 10],
                       'kernel': ['rbf', 'poly', 'sigmoid']}
-        model = SVC(random_state=self.random_state, max_iter=10000)
-        grid = GridSearchCV(model, parameters, cv=10)
+        model = SVC(random_state=self.SEED, max_iter=10000)
+        grid = GridSearchCV(model, parameters, cv=10, scoring='accuracy', return_train_score=True)
         grid.fit(x_train, y_train)
 
         model_info = ["Support Vectors Classifier", x_test, y_test, "SVC"]
@@ -166,9 +213,9 @@ class PredictPRinKP:
         x_train, x_test, y_train, y_test = self.perform_train_test_split(dataset, metadata)
 
         parameters = {'learning_rate': [0.001, 0.01, 0.1], 'n_estimators': [50, 100, 200, 300, 400, 500],
-                      'max_depth': [1, 3, 5, 10, 12]}
-        model = GradientBoostingClassifier(random_state=self.random_state)
-        grid = GridSearchCV(model, parameters, cv=10)
+                      'max_depth': [3, 5, 10, 12, 15]}
+        model = GradientBoostingClassifier(random_state=self.SEED)
+        grid = GridSearchCV(model, parameters, cv=10, scoring='accuracy', return_train_score=True)
         grid.fit(x_train, y_train)
 
         model_info = ["Gradient Boosting Classifier", x_test, y_test, "GBTC"]
@@ -178,14 +225,24 @@ class PredictPRinKP:
     def train_random_forest(self, dataset, metadata, name):
         x_train, x_test, y_train, y_test = self.perform_train_test_split(dataset, metadata)
 
-        parameters = {'n_estimators': [5, 10, 15, 20], 'max_features': ["sqrt", "log2", 0.5],
-                      'max_depth': [None], 'criterion': ['gini', 'entropy']}
-        model = RandomForestClassifier(random_state=self.random_state)
-        grid = GridSearchCV(model, parameters, cv=10)
+        parameters = {'n_estimators': [50, 100, 200, 400], 'max_features': ['sqrt', 'log2'],
+                      'max_depth': [10, 12, 15, 20], 'criterion': ['gini', 'entropy']}
+        model = RandomForestClassifier(random_state=self.SEED)
+        grid = GridSearchCV(model, parameters, cv=10, scoring='accuracy', return_train_score=True)
         grid.fit(x_train, y_train)
 
         model_info = ["Random Forest", x_test, y_test, "RF"]
         self.print_performance_metrics(grid, model_info[0], model_info[1], model_info[2], name, model_info[3])
+
+        return self
+
+    def perform_XGB(self, dataset, metadata, name):
+        # x_train, x_test, y_train, y_test = self.perform_train_test_split(dataset, metadata)
+        #
+        # train_matrix = xgb.DMatrix(x_train, y_trai n)
+        # test_matrix = xgb.DMatrix(x_test, y_test)
+        #
+        # model = xgb.train()
 
         return self
 
@@ -199,11 +256,11 @@ class PredictPRinKP:
 
 
 if __name__ == "__main__":
-    # Uncomment to genetare log.text
+    # Uncomment to genetare log.txt
     # stdoutOrigin = sys.stdout
-    # sys.stdout = open("log.txt", "w")
+    # sys.stdout = open("log_fs_SVN_50estimators.txt", "w")
 
-    df01 = ['dataset_1_', 'dataset_3_']
+    df01 = ['dataset_1_', 'dataset_2_', 'dataset_3_']
     df02 = ['dataset_19_', 'dataset_20_', 'dataset_21_']
 
     PR = PredictPRinKP(datasets_full=df01, datasets_gwas=df02)
